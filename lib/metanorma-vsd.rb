2223c5ec4d59aa1709dc020a232ecb6c93f3283a
Ronald Tse <ronald.tse@ribose.com>
2018-07-17 18:17:25 +0800

    Make runnable via metanorma-cli

--- lib/metanorma-vsd.rb
+++ lib/metanorma-vsd.rb
@@ -1,9 +1,6 @@
-require "asciidoctor" unless defined? Asciidoctor::Converter
-require_relative "metanorma/vsd/version"
-require_relative "asciidoctor/vsd"
-require_relative "isodoc/vsd"
+require "metanorma"
+require "metanorma/vsd"
 
-if defined? Metanorma
-  require_relative "metanorma/vsd"
-  Metanorma::Registry.instance.register(Metanorma::Vsd::Processor)
-end
+require "asciidoctor" unless defined? Asciidoctor::Converter
+require "asciidoctor/vsd"
+require "isodoc/vsd"
--- lib/metanorma/vsd.rb
+++ lib/metanorma/vsd.rb
@@ -1,8 +1,10 @@
 require "metanorma"
-require_relative "./vsd/processor"
+require "metanorma/vsd/processor"
 
 module Metanorma
   module Vsd
     ORGANIZATION_NAME = "Vita Green"
   end
 end
+
+Metanorma::Registry.instance.register(Metanorma::Vsd::Processor)
