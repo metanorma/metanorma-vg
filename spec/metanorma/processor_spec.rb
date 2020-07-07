314064b4537714818b476d0616d73ca160505312
Nick Nicholas <opoudjis@gmail.com>
2020-07-08 00:08:21 +1000

    section labelling: https://github.com/metanorma/metanorma-standoc/issues/285

--- spec/metanorma/processor_spec.rb
+++ spec/metanorma/processor_spec.rb
@@ -42,8 +42,9 @@ RSpec.describe Metanorma::VG::Processor do
     input = <<~"INPUT"
     <vsd-standard xmlns="https://metanorma.org/ns/vsd">
       <sections>
-        <terms id="H" obligation="normative"><title>Terms, Definitions, Symbols and Abbreviated Terms</title>
+        <terms id="H" obligation="normative"><title>1.<tab/>Terms, Definitions, Symbols and Abbreviated Terms</title>
           <term id="J">
+          <name>1.1.</name>
             <preferred>Term2</preferred>
           </term>
         </terms>
